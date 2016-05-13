7a8bbb61e8cda11c43a4348e011a62714da0e339
Andrew <andrewfb1996@gmail.com>
2016-05-13 11:51:02 +0200

    facebook request

---
+++ GaptWebsite/Models/FbData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GaptWebsite.Models
+{
+    public class FbData
+    {
+        public string name { get; set; }
+        public string description { get; set; }
+        public DateTime start_time { get; set; }
+        public Place place { get; set; }
+
+        public FbData(string nameIN, string descriptionIn, DateTime start_timeIn, Place p)
+        {
+            name = nameIN;
+            description = descriptionIn;
+            start_time = start_timeIn;
+            place = p;
+        }
+
+    }
+}
